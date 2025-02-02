4e66ad9f7dbd1671d4a5596df9749d3ecbb31a13
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-02 11:20:12 +0330

    datetime, home win

--- analysis.py
+++ analysis.py
@@ -2,4 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
+
+df['Date'] = pd.datetime(df['Date'])
+df['Home Win'] = df['Winner'] == df['Home Team']
